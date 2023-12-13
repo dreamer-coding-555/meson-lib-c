88c5bd30b76431346f334d77f9eafa518fe5f6d6
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-12-13 11:33:25 -0600

    Update xunit_runner.c

--- test/xunit_runner.c
+++ test/xunit_runner.c
@@ -42,7 +42,7 @@ XTEST_GROUP_EXTERN(basic_group);
 int main(int argc, char **argv) {
     XUnitRunner runner = XTEST_RUNNER_START(argc, argv);
 
-    XTEST_GROUP_REGISTER(basic_group, &runner);
+    XTEST_GROUP_REGISTER(basic_group, runner);
 
     return XTEST_RUNNER_END(runner);
 } // end of func
